a655eee5d30fb5bc0b4c8d19f9939926e9aaf85d
cRejon <carlosrejongomez@gmail.com>
2024-05-30 16:38:58 +0200

    Fix Arduino comments

--- arduino/examples/Board_1/LCD_Test.ino
+++ arduino/examples/Board_1/LCD_Test.ino
@@ -12,12 +12,12 @@
   the display.
 
   The circuit:
-    - LCD RS pin to digital pin 12
-    - LCD Enable pin to digital pin 11
+    - LCD RS pin to digital pin 3
+    - LCD Enable pin to digital pin 4
     - LCD D4 pin to digital pin 5
-    - LCD D5 pin to digital pin 4
-    - LCD D6 pin to digital pin 3
-    - LCD D7 pin to digital pin 2
+    - LCD D5 pin to digital pin 6
+    - LCD D6 pin to digital pin 9
+    - LCD D7 pin to digital pin 10
     - LCD R/W pin to ground
     - LCD VSS pin to ground
     - LCD VCC pin to 5V
