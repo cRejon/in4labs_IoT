ddadfe6485eb224a567b7d038b907279ff129d28
cRejon <carlosrejongomez@gmail.com>
2025-03-18 14:58:26 +0100

    Fix comments on serial monitor

--- arduino/examples/Master/Master_Blink_-_BLE.ino
+++ arduino/examples/Master/Master_Blink_-_BLE.ino
@@ -75,11 +75,11 @@ void controlLed(BLEDevice peripheral) {
   BLECharacteristic ledCharacteristic = peripheral.characteristic("19B10001-E8F2-537E-4F6C-D104768A1214");
 
   if (!ledCharacteristic) {
-    Serial.println("Peripheral does not have LED characteristic!");
+    Serial.println("Peripheral does not have this characteristic!");
     peripheral.disconnect();
     return;
   } else if (!ledCharacteristic.canWrite()) {
-    Serial.println("Peripheral does not have a writable LED characteristic!");
+    Serial.println("Peripheral does not have a writable characteristic!");
     peripheral.disconnect();
     return;
   }
