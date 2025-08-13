a44a7d0c0940a53fc7ec44744b9c729956b152c3
cRejon <carlosrejongomez@gmail.com>
2025-08-13 13:17:51 +0200

    Change host_port assignment to mountings

--- test/test_config.py
+++ test/test_config.py
@@ -6,12 +6,12 @@ class Config(object):
             'id': '1', 
             'duration': 10, # minutes
             'cam_url': 'https://ULR_TO_WEBCAM/stream.m3u8',
+            'host_port' : 8001,
         },],
         'labs': [{
             'lab_name' : 'in4labs_IoT',
             'html_name' : 'Laboratory of Internet of Things',
             'description' : 'This lab performs IoT experiments on Arduino devices',
             'mounting_id': '1',
-            'host_port' : 8001,
         }],
     }
